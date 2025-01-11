9bc6b263edbbbe382633b15ee0f7899f002ced7a
BrioyterBeams <cswbrian@gmail.com>
2025-01-11 02:29:13 -0700

    Excercise 3 complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1,101);
+        int guess = 0;
+        while (guess != magicNum)
+        {
+            Console.Write("What is the magic number? ");
+            guess = int.Parse(Console.ReadLine());
+            if (magicNum > guess)
+            {
+                Console.WriteLine("Higher.");
+            }
+            else if (magicNum < guess)
+            {
+                Console.WriteLine("Lower.");
+            }
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+        }
+
     }
 }
