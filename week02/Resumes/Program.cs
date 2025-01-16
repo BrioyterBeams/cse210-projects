58706a039d4be6b3b1e3b314ea03d128a43a8708
BrioyterBeams <cswbrian@gmail.com>
2025-01-16 15:17:19 -0700

    Abstraction Learning

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2001;
+        job1._endYear = 2002;
+
+        Job job2 = new Job();
+        job2._jobTitle = "IT Head";
+        job2._company = "Apple";
+        job2._startYear = 2003;
+        job2._endYear = 2004;
+
+        Resume myResume = new Resume();
+        myResume._name = "Brioyter Beams";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        myResume.Display();
+
+
     }
 }
